f8af51d8a2f423b53696ebb07e1d5c0c6d6b1346
Kayo campos <kayonbahenrique@gmail.com>
2022-06-11 16:31:14 -0300

    integrando firebase

---
+++ backend/config.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import 'firebase/firestore';
+
+
+if(!firebase.apps.length){
+    firebase.initializeApp({
+        apiKey: process.env.NEXT_PUCLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUCLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUCLIC_FIREBASE_PROJECT_ID,
+    })
+}
+
+
+export default firebase
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,31 +3,46 @@ import Layout from "../components/Layout";
 import Tabela from "../components/Tabela";
 import Cliente from "../core/Cliente";
 import Formulario from "../components/Formulario";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import ColecaoCliente from "../../backend/db/ColecaoCliente";
+import ClienteRepositorio from "../core/ClienteRepositorio";
 
 
 export default function Home() {
+
+
+  const repo : ClienteRepositorio = new ColecaoCliente()
   
-    const clientes = [
-      new Cliente('Ana', 34, '1'),
-      new Cliente('Bia', 45, '2'),
-      new Cliente('Carlos', 23, '3'),
-      new Cliente('Pedro', 54, '4') 
-    ]
+  const [cliente, setCliente] = useState<Cliente>(Cliente.vazio())
+  const [clientes, setClientes] = useState<Cliente[]>([])
+  const [visivel, setVisivel] = useState<'tabela'| 'form'>('tabela')
   
+  useEffect(obterTodos, [] )
+  
+
+    function obterTodos(){
+      repo.obterTodos().then(clientes => {
+        setClientes(clientes)
+        setVisivel('tabela')
+      })
+    }
+
     function clienteSelecionado(cliente: Cliente) {
-      console.log(cliente.nome)
+      setCliente(cliente) 
+      setVisivel('form')
     }
     function clienteExcluido(cliente: Cliente) {
       console.log(`Excluir... ${cliente.nome}`)
     }
-    function salvarCliente(cliente: Cliente){
-      console.log(cliente)
+   async function salvarCliente(cliente: Cliente){
+      await repo.salvar(cliente)
+      obterTodos
     }
-
-
-    const [visivel, setVisivel] = useState<'tabela'| 'form'>('tabela')
-
+    function novoCliente(){
+      setCliente(Cliente.vazio())
+      setVisivel('form')
+    }
+    
   return (
     <div className={`flex h-screen justify-center items-center bg-gradient-to-r from-purple-500  to-blue-600 `}>
      
@@ -36,12 +51,12 @@ export default function Home() {
               <>
             <div className="flex justify-end">
               <Botao className="mb-4" 
-              onClick={() => setVisivel('form')}>Novo Cliente </Botao>
+              onClick={novoCliente}>Novo Cliente </Botao>
             </div>  
               <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
               </>
         ) : (
-          <Formulario cliente={clientes[1]}
+          <Formulario cliente={cliente}
           clienteMudou={salvarCliente}
            cancelado={() => setVisivel('tabela')} 
           />
